1f4c07375d82270c4daf0e242e69890c32f623e8
Mahmoud Nasr <42423651+c7nasr@users.noreply.github.com>
2021-11-29 18:16:00 +0200

    #1

---
+++ one/main.py
@@ -0,0 +1,230 @@
+import csv
+import random
+import time
+
+
+import json
+import humanize
+import uuid
+
+import requests
+
+import urllib.parse
+from tinydb import TinyDB
+
+class Instagramify:
+    def __init__(self):
+        self.file_name = time.time()
+        self.user_agent = "Instagram 4.1.1 Android (11/1.5.0; 285; 800x1280; samsung; GT-N7000; GT-N7000; smdkc210; en_US)"
+        self.guid = str(uuid.uuid1())
+
+        self.login_url = "https://i.instagram.com/api/v1/accounts/login/"
+        self.posts_link = 'https://i.instagram.com/api/v1/feed/user/'
+        self.comments_link = 'https://www.instagram.com/graphql/query/?query_hash=bc3296d1ce80a24b1b6e40b1e72903f5&variables={}'
+
+        self.posts_data = []
+        self.db =  TinyDB('{}.json'.format(self.file_name))
+        self.loop_targets()
+
+
+    def loop_targets(self):
+        with open('targets.txt', 'r') as f:
+            links = [line.strip() for line in f]
+        f.close()
+        links = list(filter(None, links))
+        for (i,link) in enumerate(links):
+            print("We starting with #{}",format(i+1))
+            self.current_url = link
+            self.login(self.current_url)
+            
+    def login(self,link,is_rate=False):
+        self.session = requests.Session()
+        self.session.headers.update(
+            {'User-Agent': self.user_agent, 'Content-Type': 'application/x-www-form-urlencoded', })
+        text_accounts = open("accounts.txt",'r')
+        accounts = [line.strip() for line in text_accounts]
+        accounts = list(filter(None, accounts))
+        random_account = random.choices(accounts)[0]
+
+        username = random_account.split(":")[0]
+        password = random_account.split(":")[1]
+        print("Trying with ID:{}".format(username))
+        payload = 'password={}&username={}&device_id={}'.format(password,username,self.guid)
+        login_response = self.session.post(self.login_url, payload)
+        login_response = login_response.json()
+        if login_response['logged_in_user']:
+            print("Logged in with ID:{}".format(username))
+            self.basic_information(link)
+        else:
+            print("Something Went Wrong while logging in.... USER:{}".format(username))
+            exit()
+
+    def basic_information(self, link):
+        print("Getting Basic Public Information")
+        r = self.session.get("{}?__a=1".format(link))
+        r = r.json()['graphql']['user']
+
+        self.user_data = {
+            "followed": humanize.intcomma(r['edge_followed_by']['count']),
+            "following": humanize.intcomma(r['edge_follow']['count']),
+            "posts": humanize.intcomma(r['edge_owner_to_timeline_media']['count']),
+            "is_verified": r['is_verified'],
+            "full_name": r['full_name'],
+            "user_id": r['id'],
+            "username": r['username'],
+            "link": link
+        }
+        users = self.db.table("users")
+        users.insert(self.user_data)
+        print(self.user_data)
+        print("Basic Information Done")
+        self.start_post_fetch(self.user_data)
+
+    def start_post_fetch(self, data):
+        num_results = 0
+        self.posts_data = []
+        # GET first 18
+        r = self.session.get(self.posts_link + data['user_id']).json()
+        num_result = r['num_results']
+        next_id = None
+        if num_result > 1:
+            if "next_max_id"  in r:
+                next_id = r['next_max_id']
+            
+            self.posts_data.append(self.fetch_post_data(r))
+            num_results += num_result
+            while num_results < 100 and next_id is not None:
+                # GET 82
+                r_2 = self.session.get(self.posts_link + data['user_id'] + "/?max_id=" + next_id).json()
+                next_id = r_2['next_max_id']
+                num_result = r_2['num_results']
+                self.posts_data.append(self.fetch_post_data(r_2))
+                num_results += num_result
+                print(num_results)
+            self.posts_data = [x for x in self.posts_data if x is not None]
+            self.posts_data = self.posts_data[:100]
+            print(len(self.posts_data))
+            self.fetch_post_comments()
+
+    def fetch_post_data(self, json_response):
+        posts = self.db.table("posts")
+        for i in json_response['items']:
+            if i is not None:
+                caption = "No caption detected"
+                if i['caption']:
+                    caption = i['caption']['text']
+                post_object = {
+                    "id": i['id'],
+                    "likes": i['like_count'],
+                    "comments_count": i['comment_count'],
+                    "code": i['code'],
+                    "caption": caption,
+                    "comments": [],
+                    "user": self.user_data,
+
+                }
+                self.posts_data.append(post_object)
+                posts.insert(post_object)
+
+
+    def fetch_post_comments(self, ):
+        
+        for (post_id,i) in enumerate(self.posts_data):
+            try:
+                variable = self.comments_object_convertor(i['code'])
+                r = self.session.get(self.comments_link.format(variable)).json()
+                if r['status'] != "fail":
+                    comments_we_have = 0
+                    page_info_data = r['data']['shortcode_media']['edge_media_to_parent_comment']
+                    count = len(page_info_data['edges'])
+                    
+                    is_has_more = page_info_data['page_info']['has_next_page']
+                    is_has_more_hash = False
+                    if is_has_more and count != 0:
+                        is_has_more_hash = page_info_data['page_info']['end_cursor']
+                    print("Post #{} Comments Count {}".format(i['code'],count))
+                    print("Post #{} has more Comments? {}".format(i['code'],is_has_more))
+                    
+                    for comment in page_info_data['edges']:
+                        self.comments_appender(comment,i)
+                        comments_we_have +=1
+                    print("We have {} comments of {} : {} #{}".format(comments_we_have,count+comments_we_have, i['code'],post_id))
+                    while is_has_more and count != 0:
+                        
+                        time.sleep(3)
+                        variable = self.comments_object_convertor(i['code'], is_has_more_hash)
+                        print(variable)
+                        r = self.session.get(self.comments_link.format(variable)).json()
+                        page_info_data = r['data']['shortcode_media']['edge_media_to_parent_comment']
+                        count = len(page_info_data['edges'])
+                        is_has_more = page_info_data['page_info']['has_next_page']
+                        is_has_more_hash = page_info_data['page_info']['end_cursor']
+                        print("Post #{} Comments Count {}".format(i['code'],count))
+                        print("Post #{} has more Comments?".format(i['code']), is_has_more)
+                        for comment in page_info_data['edges']:
+                            self.comments_appender(comment,i)
+                            comments_we_have +=1
+                        print("We have {} comments of {} : {} #{}".format(comments_we_have,count+comments_we_have, i['code'],post_id))
+                        time.sleep(3)
+                    time.sleep(3)
+                else:
+                    print("ERROR: from INSTAGRAM API or Account limited... trying other account")
+                    self.login(self.current_url,is_rate=True)
+            except Exception as e:
+                print(i)
+                print("ERROR: ", e)
+                print("ERROR: from INSTAGRAM API or Account limited..... trying to Logging in with other account")
+                self.login(self.current_url,is_rate=True)
+                continue
+        print("Fetching Comments of Post #{}".format(post_id+1))
+        self.save_results()
+
+
+    def comments_appender(self,comment,i):
+    
+        comments = self.db.table("comments")
+        comment_object = {
+                            "comment_id": comment['node']['id'],
+                            "user_commented": comment['node']['owner']['username'],
+                            "user_link": 'https://instagram.com/{}'.format(comment['node']['owner']['username']),
+                            "is_verified": comment['node']['owner']['is_verified'],
+                            "comment": comment['node']['text']
+                        }
+        i['comments'].append(comment_object)
+        comments.insert(comment_object)
+
+       
+        
+    def comments_object_convertor(self, code, after=None):
+        if after:
+            variable = {"shortcode": code, "first": 50, "after": after}
+        else:
+            variable = {"shortcode": code, "first": 50}
+        j = json.dumps(variable)
+        return urllib.parse.quote_plus(j)
+
+    def is_rate_limited(self, response):
+        if response['status'] == "fail" or response['message'] == "feedback_required":
+            print("This account is limited. trying another account....")
+
+    def save_results(self):
+        with open('{}.csv'.format(self.file_name), 'a', newline='', encoding="utf-8-sig") as Saver:
+            results_writer = csv.writer(Saver)
+            for p in self.posts_data:
+                    try:
+
+                        for c in p['comments']:
+                            if p['user']:
+                                results_writer.writerow(
+                                    [p['user']['username'], p['user']['link'], p['user']['posts'], p['user']['following'],
+                                     p['user']['followed'], p['caption'], p['likes'], c['user_commented'], c['comment'], c['user_link'],
+                                     c['is_verified']])
+                    except Exception as e:
+                        print("ERROR: Saving file error, ",e)
+                        continue
+            self.posts_data = []
+        Saver.close()
+
+
+if __name__ == '__main__':
+    app = Instagramify()
---
+++ two/main.py
@@ -0,0 +1,157 @@
+import csv
+from os import path
+import random
+import time
+
+
+import json
+import humanize
+import uuid
+from humanize.number import intcomma
+
+import requests
+
+import urllib.parse
+from tinydb import TinyDB,Query
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+class Commentify:
+    def __init__(self):
+        self.file_name = time.time()
+        self.user_agent = "Instagram 4.1.1 Android (11/1.5.0; 285; 800x1280; samsung; GT-N7000; GT-N7000; smdkc210; en_US)"
+        self.guid = str(uuid.uuid1())
+
+        self.login_url = "https://i.instagram.com/api/v1/accounts/login/"
+        self.followers_url = "https://i.instagram.com/api/v1/friendships/{}/followers/?count={}&search_surface=follow_list_page"
+        Tk().withdraw()
+        db_path = askopenfilename(initialdir=".",
+                           filetypes=(("Database File", "*.json"),),
+                           title="Choose a file."
+                           )
+        self.db = TinyDB(db_path)
+        self.commenters_db = TinyDB("{}.json".format(self.file_name))
+
+        self.commenters = []
+
+        self.SearchInDB = Query()
+        self.login()
+        
+     
+    def login(self):
+        self.session = requests.Session()
+        self.session.headers.update(
+            {'User-Agent': self.user_agent, 'Content-Type': 'application/x-www-form-urlencoded'})
+        text_accounts = open("accounts.txt",'r')
+        accounts = [line.strip() for line in text_accounts]
+        accounts = list(filter(None, accounts))
+        random_account = random.choices(accounts)[0]
+
+        username = random_account.split(":")[0]
+        password = random_account.split(":")[1]
+        print("Trying with ID:{}".format(username))
+        payload = 'password={}&username={}&device_id={}'.format(password.strip(),username.strip(),self.guid)
+        login_response = self.session.post(self.login_url, payload)
+        login_response = login_response.json()
+        if login_response['logged_in_user']:
+            print("Logged in with ID:{}".format(username))
+            self.get_comments()
+        else:
+            print("Something Went Wrong while logging in.... USER:{}".format(username))
+            exit()
+
+    def get_comments(self):
+        all_comments = self.db.table("comments").all()
+        if len(all_comments) > 0:
+            print("We have got {} comment to look into it!".format(humanize.intcomma(len(all_comments))))
+            for i,comment in enumerate(all_comments):
+                self.get_commenter_public_info(comment,i)
+                
+            print("Actual Number of Commenters (Remove Duplicates) #{}".format(len(self.commenters)))
+            self.get_followers_info()
+            
+
+        else:
+            print("Whops! we did not see any comments")
+            exit()
+
+
+    def get_commenter_public_info(self,comment,i):
+        try:
+            
+            is_exitsted = self.commenters_db.table("commenters").search(self.SearchInDB.username == comment['user_commented'])
+            if len(is_exitsted) == 0:
+                r = self.session.get("https://instagram.com/{}?__a=1".format(comment['user_commented']))
+                r = r.json()['graphql']['user']
+
+                commenter = {
+                    "followedBy": humanize.intcomma(r['edge_followed_by']['count']),
+                    "following_count": humanize.intcomma(r['edge_follow']['count']),
+                    "posts": humanize.intcomma(r['edge_owner_to_timeline_media']['count']),
+                    "is_verified": r['is_verified'],
+                    "full_name": r['full_name'],
+                    "user_id": r['id'],
+                    "username": r['username'],
+                    "followers":[],
+                    "is_private":r['is_private']
+                }
+                self.commenters_db.table("commenters").insert(commenter)
+                self.commenters.append(commenter)
+                print("Commenter #{} Public Info Ready.".format(i+1))
+                time.sleep(1)
+            else:
+                print("Skipping commenter due to duplicate")
+        except KeyError as e:
+            print("ERROR: Key error on {}".format(comment['user_commented']))
+            
+        
+
+    def get_followers_info(self):
+            
+            for (i,commenter) in enumerate(self.commenters):
+                    try:
+                        if commenter['is_private'] == False:
+                            print("Commenter #{} of {}".format(i+1, intcomma(len(self.commenters))))
+                            followers_list = self.session.get(self.followers_url.format(commenter['user_id'],commenter['following_count'])).json()
+                            data = followers_list['users']
+                            count_following_from_api = len(data)
+                            print("Commenter with id: {} followers #{}".format(commenter['username'],humanize.intcomma(count_following_from_api)))
+                            for f in data:
+                                follower = {
+                                "username":f['username'],
+                                "link":"https://instagram/{}".format(f['username']),
+                                "is_verified":f['is_verified'],
+                                "origin_user":commenter['username']
+                                }
+                                commenter['followers'].append(follower)
+                                self.commenters_db.table("followers").insert(follower)
+                            time.sleep(3)
+                        else:
+                            print("Skipping {} due to private account".format(commenter['username']))
+                        
+                    except Exception as e:
+                        print(e)
+                        print("ERROR: Something wrong ", e)
+                        print("Located ERROR:", i)
+                        continue
+            self.save_results()
+
+    def save_results(self):
+        with open('{}.csv'.format(self.file_name), 'a', newline='', encoding="utf-8-sig") as Saver:
+            results_writer = csv.writer(Saver)
+            for commenter in self.commenters:
+                    try:
+
+                        for f in commenter['followers']:
+                            if commenter['user_id']:
+                                results_writer.writerow(
+                                    [commenter['username'],  commenter['posts'], commenter['followedBy'],
+                                     commenter['following_count'], f['username'], f['link'],f['is_verified']])
+                    except Exception as e:
+                        print("ERROR: Saving file error, ",e)
+                        continue
+            self.commenters = []
+        Saver.close()         
+if __name__ == '__main__':
+    app = Commentify()
+        
